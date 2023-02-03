6ebac6c30ea4e6f9863b1ee8109f2a87ed4d3331
Wycliff <w4wycliff@gmail.com>
2023-02-03 21:22:28 +0300

    Add json-server

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
-	<div class="container">
-		<Header @toggle-add-task="toggleAddTask" title="Task Tracker"  :showAddTask="showAddTask" />
-		<div v-show="showAddTask">
-			<AddTask @add-task="addTask" />
-		</div>
+  <div class="container">
+    <Header
+      @toggle-add-task="toggleAddTask"
+      title="Task Tracker"
+      :showAddTask="showAddTask"
+    />
+    <div v-show="showAddTask">
+      <AddTask @add-task="addTask" />
+    </div>
 
-		<Tasks
-			@toggle-reminder="toggleReminder"
-			@delete-task="deleteTask"
-			:tasks="tasks" />
-	</div>
+    <Tasks
+      @toggle-reminder="toggleReminder"
+      @delete-task="deleteTask"
+      :tasks="tasks"
+    />
+  </div>
 </template>
 
 <script>
@@ -18,90 +23,116 @@ import Tasks from "./components/Tasks.vue";
 import AddTask from "./components/AddTask.vue";
 
 export default {
-	name: "App",
-	components: { Header, Tasks, AddTask },
-	data() {
-		return {
-			tasks: [],
-			showAddTask: false
-		};
-	},
-	methods: {
+  name: "App",
+  components: { Header, Tasks, AddTask },
+  data() {
+    return {
+      tasks: [],
+      showAddTask: false,
+    };
+  },
+  methods: {
     toggleAddTask() {
-      this.showAddTask = !this.showAddTask
+      this.showAddTask = !this.showAddTask;
     },
-		addTask(task) {
-			this.tasks = [...this.tasks, task];
-		},
-		deleteTask(id) {
-			if (confirm("Are you sure?")) {
-				this.tasks = this.tasks.filter((task) => task.id !== id);
-			}
-		},
-		toggleReminder(id) {
-			this.tasks = this.tasks.map((task) =>
-				task.id === id ? { ...task, reminder: !task.reminder } : task
-			);
-		}
-	},
-	created() {
-		this.tasks = [
-			{
-				id: 1,
-				text: "Doctors Appointment",
-				day: "March 1st at 2:30pm",
-				reminder: true
-			},
-			{
-				id: 2,
-				text: "Meeting at School",
-				day: "March 3rd at 1:30pm",
-				reminder: false
-			}
-		];
-	}
+    async addTask(task) {
+      const res = await fetch("api/tasks", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(task),
+      });
+
+      const data = await res.json();
+
+      this.tasks = [...this.tasks, data];
+    },
+    async deleteTask(id) {
+      if (confirm("Are you sure?")) {
+        const res = await fetch(`api/tasks/${id}`, {
+          method: "DELETE",
+        });
+
+        res.status === 200
+          ? (this.tasks = this.tasks.filter((task) => task.id !== id))
+          : alert("Error deleting task");
+      }
+    },
+    async toggleReminder(id) {
+      const taskToToggle = await this.fetchTask(id);
+      const updateTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+
+      const res = await fetch(`api/tasks/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(updateTask),
+      });
+
+      const data = await res.json();
+
+      this.tasks = this.tasks.map((task) =>
+        task.id === id ? { ...task, reminder: data.reminder } : task
+      );
+    },
+    async fetchTasks() {
+      const res = await fetch("api/tasks");
+      const data = res.json();
+      return data;
+    },
+    async fetchTask(id) {
+      const res = await fetch(`api/tasks/${id}`);
+      const data = res.json();
+      return data;
+    },
+  },
+  async created() {
+    this.tasks = await this.fetchTasks();
+  },
 };
 </script>
 
 <style>
 * {
-	box-sizing: border-box;
-	margin: 0;
-	padding: 0;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
 body {
-	font-family: "Poppins", sans-serif;
+  font-family: "Poppins", sans-serif;
 }
 .container {
-	max-width: 500px;
-	margin: 30px auto;
-	overflow: auto;
-	min-height: 300px;
-	border: 1px solid steelblue;
-	padding: 30px;
-	border-radius: 5px;
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
 }
 .btn {
-	display: inline-block;
-	background: #000;
-	color: #fff;
-	border: none;
-	padding: 10px 20px;
-	margin: 5px;
-	border-radius: 5px;
-	cursor: pointer;
-	text-decoration: none;
-	font-size: 15px;
-	font-family: inherit;
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
 }
 .btn:focus {
-	outline: none;
+  outline: none;
 }
 .btn:active {
-	transform: scale(0.98);
+  transform: scale(0.98);
 }
 .btn-block {
-	display: block;
-	width: 100%;
+  display: block;
+  width: 100%;
 }
 </style>
